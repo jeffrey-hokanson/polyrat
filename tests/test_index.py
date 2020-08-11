1967bcbe0f7c5dde0b8a39b9e23d42a236725833
Jeffrey Hokanson <jeffrey@hokanson.us>
2020-08-10 19:11:45 -0600

    Added a specific test for max_degree

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 from polyrat import total_degree_index, max_degree_index
+from itertools import product
 
 
 def total_degree_index_slow(dim, degree):
@@ -21,5 +22,16 @@ def test_total_degree_index(dim, degree):
 		res = (true_indices == indices[k]).all(axis = 1).nonzero()
 		assert len(res[0]) == 1, "Should only be one match"
 
+
+def test_max_degree_index(degree):
+	indices = max_degree_index(degree)
+
+	for idx in product(*[range(d+1) for d in degree]):
+		res = (indices == idx).all(axis = 1).nonzero()
+		assert len(res[0]) == 1, "Should only be one match"
+		 
+
+
 if __name__ == '__main__':
-	test_total_degree_index(dim=3, degree=5)
+#	test_total_degree_index(dim=3, degree=5)
+	test_max_degree_index([1,2,3])
