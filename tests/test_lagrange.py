66911fab811aab33c43ed58d7ac9744d997a1463
Jeffrey Hokanson <jeffrey@hokanson.us>
2020-09-08 16:55:26 -0600

    Changed example to get accurate poles

--- tests/test_lagrange.py
+++ tests/test_lagrange.py
@@ -4,12 +4,13 @@ import pytest
 
 
 @pytest.mark.parametrize("n", [5, 10, 15, 20])
-def test_roots_wilkinson(n):
+@pytest.mark.parametrize("ang", [1, (1 +1j)/np.sqrt(2)])
+def test_roots_wilkinson(n, ang):
 	r""" Test computation of barycentric polynomial roots using the Wilkinson polynomial
 
 	This test is the first one 
 	"""
-	true_roots = np.arange(1, n+1)
+	true_roots = ang* np.arange(1, n+1)
 	def wilkinson(x):
 		value = np.zeros(x.shape, dtype = np.complex)
 		for i, xi in enumerate(x):
@@ -18,7 +19,7 @@ def test_roots_wilkinson(n):
 
 
 	# First try with basis sampling at nodes
-	X = np.arange(0,n+1)
+	X = ang*np.arange(0,n+1)
 	lpi = LagrangePolynomialInterpolant(X, wilkinson(X))
 	roots = lpi.roots()
 	print(roots)		
@@ -27,15 +28,19 @@ def test_roots_wilkinson(n):
 	print(err)
 	assert err < 1e-10, "Could not determine roots accurately"
 
-	# Now spaced around a scaled unit circle (see  LC14, Sec.~4.1 )
-	X = n*np.exp(1j*np.linspace(0,2*np.pi, n+1, endpoint = False))
+	# Now shifted slightly off the poles (see  LC14, Sec.~4.4 )
+	#X = (n+1)*np.exp(1j*np.linspace(0,2*np.pi, n+1, endpoint = False))
+	X = ang*np.arange(0.5, 1.5 +n)
 	lpi = LagrangePolynomialInterpolant(X, wilkinson(X))
 	roots = lpi.roots()
 	print(roots)		
 	print(true_roots)
+	print(true_roots - roots)
 	err = sorted_norm(roots, true_roots, np.inf)	
 	print(err)
 	assert err < 1e-10, "Could not determine roots accurately"
+	
+
 
 if __name__ == '__main__':
-	test_roots_wilkinson(5)
+	test_roots_wilkinson(20, 1+1j)
