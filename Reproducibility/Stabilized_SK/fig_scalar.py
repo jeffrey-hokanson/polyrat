abe5ef05b38202da3096fb71c920a577111e4e12
Jeffrey Hokanson <jeffrey@hokanson.us>
2020-09-16 21:02:52 -0600

    Final version of data

--- Reproducibility/Stabilized_SK/fig_scalar.py
+++ Reproducibility/Stabilized_SK/fig_scalar.py
@@ -17,8 +17,8 @@ y = np.abs(X).flatten()
 data += [['abs', X, y]]
 
 # Exponential (NST18: Sec. 6.8)
-M = 8000
-X = -np.logspace(-6, 6, M).reshape(-1,1)
+M = 2000
+X = -np.logspace(-3, 4, M).reshape(-1,1)
 y = np.exp(X.flatten())
 data += [['exp', X, y]]
 
@@ -103,8 +103,8 @@ y = np.array([ C @ np.linalg.solve(z*np.eye(A.shape[0]) - A, B)  for z in X]).fl
 data += [['beam', X.reshape(-1,1), y]]
 
 # Pick which experiments to run
-data = [d for d in data if d[0] in ['abs', 'exp' 'beam', 'tan256']]
-#data = [d for d in data if d[0] in ['exp']]
+#data = [d for d in data if d[0] in ['abs', 'exp', 'beam', 'tan256']]
+data = [d for d in data if d[0] in ['exp']]
 
 # Range of parameters
 mns = [(k,k) for k in range(2, 51, 1)]
