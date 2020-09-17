deb8839cbcc6a1adae73652f8b7ef5415486eae5
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2020-09-17 13:38:04 -0600

    New numerical experiment

--- Reproducibility/Stabilized_SK/fig_exp.py
+++ Reproducibility/Stabilized_SK/fig_exp.py
@@ -1,10 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from polyrat import *
+from pgf import PGF, save_contour
 
 np.random.seed(0)
 X = np.random.uniform(-1,1, size = (1000,2))
 y = np.exp(X[:,0]*X[:,1])/( (X[:,0]**2 - 1.2**2) * (X[:,1]**2 - 1.2**2))
+
+
+pgf = PGF()
+pgf.add('x1', X[:,0])
+pgf.add('x2', X[:,1])
+pgf.write('data/fig_exp_points.dat')
+
 
 lra = LinearizedRationalApproximation(20, 20)
 lra.fit(X, y)
@@ -12,7 +20,7 @@ lra.fit(X, y)
 ssk = SKRationalApproximation(20, 20, refine = False)
 ssk.fit(X, y)
 
-X = np.linspace(-1.5,1.5,200)
+X = np.linspace(-1.5,1.5,800)
 Y = X.copy()
 X, Y = np.meshgrid(X, Y)
 
@@ -23,9 +31,13 @@ denom2 = ssk.denominator(XX)
 denom2 /= np.max(denom2)
 
 fig, ax = plt.subplots(1,2)
-ax[0].contour(X, Y, denom1.reshape(X.shape), levels = np.linspace(-1,1,11))
-ax[0].contour(X, Y, denom1.reshape(X.shape), levels = [0], colors = 'red')
-ax[1].contour(X, Y, denom2.reshape(X.shape), levels = np.linspace(-1,1,11))
-ax[1].contour(X, Y, denom2.reshape(X.shape), levels = [0], colors = 'red')
+#ax[0].contour(X, Y, denom1.reshape(X.shape), levels = np.linspace(-1,1,11))
+p0 = ax[0].contour(X, Y, denom1.reshape(X.shape), levels = [0], colors = 'red')
+#ax[1].contour(X, Y, denom2.reshape(X.shape), levels = np.linspace(-1,1,11))
+p1 = ax[1].contour(X, Y, denom2.reshape(X.shape), levels = [0], colors = 'red')
+
+save_contour('data/fig_exp_lra.dat', p0, simplify = 1e-3)
+save_contour('data/fig_exp_ssk.dat', p1, simplify = 1e-3)
+
 
 plt.show()
