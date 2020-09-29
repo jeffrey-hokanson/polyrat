7552e16cb6d7baa125c68155c22405552ee6bac7
Jeffrey Hokanson @ Thor <jeffrey@hokanson.us>
2020-09-29 11:18:22 -0600

    Single sourcing version number

--- polyrat/__init__.py
+++ polyrat/__init__.py
@@ -1,3 +1,11 @@
+from importlib.metadata import version, PackageNotFoundError
+
+# Bring in the version number
+try:
+	__version__ = version(__name__)
+except PackageNotFoundError:
+	# package is not installed
+	pass
 
 # Utilities
 from .sorted_norm import *
