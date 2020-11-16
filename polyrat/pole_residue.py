da4f5cf55176a5a277be5f8698d71590b51098df
Jeffrey Hokanson <jeffrey@hokanson.us>
2020-11-16 12:55:25 -0700

    working on fixing bugs in pole residue

--- polyrat/pole_residue.py
+++ polyrat/pole_residue.py
@@ -1,18 +1,47 @@
 r""" Pole-residue rational function parameterization
 """
+from itertools import product
 
 import numpy as np
 from .aaa import _build_cauchy as build_cauchy
 
-def residual_jacobian_real(x, Y, lam, a, V, c, jacobian = False):
-	r"""
+def residual_jacobian_real(x, Y, V, lam, a, d, jacobian = False):
+	r""" This implementation requires all data to be real: x, Y, lam, a, V, d.
 
 	"""
 
 	C = build_cauchy(x, lam)
 
-	return 	
-	
-	
+	residual = Y.flatten() - np.einsum('ij,j...->i...', C, a).flatten()
+	residual -= np.einsum('ij,j...->i...', V, d).flatten()
 
+	if not jacobian: 
+		return residual
 
+	# Column-wise derivative of the Cauchy matrix	
+	Cp = C**2
+
+	# derivative with respect to lambda
+	#J = [np.kron(a, Cp[:,j]).flatten() for j in range(len(lam))] 
+	#J = np.column_stack(J)
+	#J = [np.kron(a, Cp[:,j]).flatten() for j in range(len(lam))] 
+	#J = np.einsum('ij,j...->i...j', C, a).reshape(-1,len(lam))
+
+	# Derivative with respect to lambda
+	J = [-np.kron(Cp[:,j], a[j]).flatten() for j in range(len(lam))]
+
+	output_dim = a.shape[1:]
+
+	if len(output_dim) == 0:
+		J += [ -C, -V]
+	else:
+		dim = int(np.prod(output_dim))
+		J += [ -np.kron(C[:,j], e.reshape(*a.shape[1:])).flatten() 
+				for j, e in product(range(len(lam)), np.eye(dim))
+			]
+		J += [ -np.kron(V[:,j], e.reshape(*d.shape[1:])).flatten() 
+				for j, e in product(range(V.shape[1]), np.eye(dim))
+			]
+	jacobian = np.column_stack(J)
+
+	return residual, jacobian
--- tests/test_pole_residue.py
+++ tests/test_pole_residue.py
@@ -1,33 +1,84 @@
 import numpy as np
 from polyrat import *
 from polyrat.pole_residue import *
+from checkjac import *
+
+import pytest
 
 
-def test_residual_real():
-	np.random.seed(0)
-	M = 1000
-	p, m = 1, 1	
-	x = np.linspace(-1,1, M)
-	Y = np.random.randn(M, 1)
-
-	lam = np.array([-0.1, -0.2])
-	a = np.array([1,-1])
-	V = np.zeros((M,0))
-	c = np.zeros((0))
-
-	r = residual_jacobian_real(x, Y, lam, a, V, c, jacobian = False) 
-
-	r_true = np.copy(Y)
+def slow_residual(x, Y, V, lam, a, c):
+	r = np.copy(Y)
+	if len(r.shape) == 1:
+		r = r.reshape(-1,1)
+		a = a.reshape(-1,1)
+		c = c.reshape(-1,1)
 
 	for i in range(Y.shape[0]):
-		for idx in np.ndindex(Y.shape[1:]):
+		for idx in np.ndindex(r.shape[1:]):
 			for j in range(len(lam)):
-				r_true[i, idx] -= a[j]/(x[i] - lam[j])
+				r[i, idx] -= a[j, idx]/(x[i] - lam[j])
 			for j in range(len(c)):
-				r_true[i,idx] -= V[i,j]*c[j]
+				r[i,idx] -= V[i,j]*c[j, idx]
 
-	print(r_true)	
+	return r.flatten()
+
+
+@pytest.mark.parametrize("output_dim",[
+		(),
+		(1,),
+		(2,),
+		(3,2), 
+	])
+def test_residual_jacobian_real(output_dim):
+	np.random.seed(0)
+	M = 1000
+	x = np.linspace(-1,1, M)
+	Y = np.random.randn(M, *output_dim)
+
+	lam = np.array([-0.1, -0.2])
+	a = np.random.randn(len(lam), *output_dim)
+	V = np.random.randn(M, 5)
+	c = np.random.randn(V.shape[1], *output_dim)
+
+	r = residual_jacobian_real(x, Y, V, lam, a, c, jacobian = False) 
+	r_true = slow_residual(x, Y, V, lam, a, c)
+
+	err = r - r_true.flatten()
+	print(r[:5])
+	print(r_true[:5])
+	err_norm = np.max(np.abs(err))
+	print("residual error", err_norm)
+	assert err_norm < 1e-10, "Residual did not compute the correct quantity"
+
+
+	# Now check the residual
+
+	forward = lambda lam, a, c : np.hstack([lam, a.flatten(), c.flatten()])
+	inverse = lambda xx: (
+		xx[:len(lam)], 
+		xx[len(lam):(len(lam)+len(a.flatten()))].reshape(*a.shape), 
+		xx[(len(lam)+len(a.flatten())):].reshape(c.shape)
+		)
+
+	xx0 = forward(lam, a, c)
+	lam0, a0, c0 = inverse(xx0)
+	assert np.all(np.isclose(lam0, lam))
+	assert np.all(np.isclose(a0, a))
+	assert np.all(np.isclose(c0, c))
+
+	res = lambda xx: residual_jacobian_real(x, Y, V, *inverse(xx), jacobian = False)
+	jac = lambda xx: residual_jacobian_real(x, Y, V, *inverse(xx), jacobian = True)[1]
+
+
+	print("------- Checking the Jacobian ---------")
+
+	err = check_jacobian(xx0, res, jac, relative = True)
+
+	assert err < 1e-5, "inaccurate Jacobian"
+
+	
+
 
 if __name__ == '__main__':
-	test_residual_real()
+	test_residual_jacobian_real()
 
