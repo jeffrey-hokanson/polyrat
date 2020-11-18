Fixed bug in pole-residue jacobian

polyrat/pole_residue.py
@@ -19,27 +19,20 @@ def residual_jacobian_real(x, Y, V, lam, a, d, jacobian = False):
 		return residual
 
 	# Column-wise derivative of the Cauchy matrix	
-	Cp = C**2
-
-	# derivative with respect to lambda
-	#J = [np.kron(a, Cp[:,j]).flatten() for j in range(len(lam))] 
-	#J = np.column_stack(J)
-	#J = [np.kron(a, Cp[:,j]).flatten() for j in range(len(lam))] 
-	#J = np.einsum('ij,j...->i...j', C, a).reshape(-1,len(lam))
 
 	# Derivative with respect to lambda
-	J = [-np.kron(Cp[:,j], a[j]).flatten() for j in range(len(lam))]
-
+	J = [-np.einsum('i,...->i...', C[:,j]**2, a[j]).flatten() for j in range(len(lam))]
+	
 	output_dim = a.shape[1:]
 
 	if len(output_dim) == 0:
 		J += [ -C, -V]
 	else:
 		dim = int(np.prod(output_dim))
-		J += [ -np.kron(C[:,j], e.reshape(*a.shape[1:])).flatten() 
+		J += [ -np.einsum('i,...->i...', C[:,j], e.reshape(output_dim)).flatten() 
 				for j, e in product(range(len(lam)), np.eye(dim))
 			]
-		J += [ -np.kron(V[:,j], e.reshape(*d.shape[1:])).flatten() 
+		J += [ -np.einsum('i,...->i...', V[:,j], e.reshape(output_dim)).flatten() 
 				for j, e in product(range(V.shape[1]), np.eye(dim))
 			]
 	jacobian = np.column_stack(J)

tests/test_pole_residue.py
@@ -8,21 +8,20 @@ import pytest
 
 def slow_residual(x, Y, V, lam, a, c):
 	r = np.copy(Y)
+
+	M = len(x)
+
 	if len(r.shape) == 1:
-		r = r.reshape(-1,1)
-		a = a.reshape(-1,1)
-		c = c.reshape(-1,1)
+		idx_iter = [[],]
+	else:
+		idx_iter = np.ndindex(r.shape[1:])
 
-	if len(
-	inside_shape = r.shape[1:]
-
-	for i in range(Y.shape[0]):
-		for idx in np.ndindex(inside_shape):
-			print(i, idx)
+	for idx in idx_iter:
+		for i in range(M):
 			for j in range(len(lam)):
-				r[i, idx] -= a[j, idx]/(x[i] - lam[j])
+				r[tuple([i, *idx])] -= a[tuple([j, *idx])]/(x[i] - lam[j])
 			for j in range(len(c)):
-				r[i, idx] -= V[i,j]*c[j, idx]
+				r[tuple([i, *idx])] -= c[tuple([j, *idx])] * V[i,j]
 
 	return r.flatten()
 
@@ -32,26 +31,25 @@ def slow_residual(x, Y, V, lam, a, c):
 		(1,),
 		(2,),
 		(3,1), 
+		(3,2,1),
 	])
 def test_residual_jacobian_real(output_dim):
 	np.random.seed(0)
-	M = 1000
+	M = 10
 	x = np.linspace(-1,1, M)
 	Y = np.random.randn(M, *output_dim)
 
-	lam = np.array([-0.1, -0.2])
+	lam = np.array([-0.1, -0.1])
 	a = np.random.randn(len(lam), *output_dim)
 	V = np.random.randn(M, 5)
-	c = np.random.randn(V.shape[1], *output_dim)
-	print("a", a.shape)
-	print("c", c.shape)
+	c = 0*np.random.randn(V.shape[1], *output_dim)
 
 	r = residual_jacobian_real(x, Y, V, lam, a, c, jacobian = False) 
 	r_true = slow_residual(x, Y, V, lam, a, c)
 
 	err = r - r_true.flatten()
-	print(r[:5])
-	print(r_true[:5])
+	print("fast", r[:5])
+	print("slow", r_true[:5])
 	err_norm = np.max(np.abs(err))
 	print("residual error", err_norm)
 	assert err_norm < 1e-10, "Residual did not compute the correct quantity"
@@ -82,9 +80,8 @@ def test_residual_jacobian_real(output_dim):
 
 	assert err < 1e-5, "inaccurate Jacobian"
 
-	
-
 
 if __name__ == '__main__':
-	test_residual_jacobian_real()
+	test_residual_jacobian_real((2,1))
+
 